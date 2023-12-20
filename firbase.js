5ca93e692f1ac7e627c744bc456cdfda6f5e9471
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-12-19 21:19:13 -0500

    initial commit

---
+++ firbase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2yd_XXz84BXJqV_oceQn8aTUQ2wWAZC8",
+    authDomain: "chat-app-cdb60.firebaseapp.com",
+    projectId: "chat-app-cdb60",
+    storageBucket: "chat-app-cdb60.appspot.com",
+    messagingSenderId: "223265773553",
+    appId: "1:223265773553:web:dfc34f47f6ddefac65d650"
+};
