14c330b93199ecb29b7553ae52cbc5c5b97265e3
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-12-22 14:58:22 -0500

    having chatgpt check the code

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ViewBase } from 'react-native';
 import React from 'react';
 import { Button, Input, Image } from '@rneui/themed';
 import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View>
       <StatusBar style='light' />
       <Image 
-        source={{
-          uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-        }}
+        source={require('../assets/2.png')}
+        alt='Logo'
         style={{ width: 200, height: 200 }} 
       />
+      <View style={styles.inputContainer}>
+        <Input 
+
+        
+          placeholder='Email' 
+          autoFocus type='email' 
+          value={email} 
+          onChange={(text) => setEmail(text)} 
+        />
+        <Input 
+          placeholder='Password' 
+          secureTextEntry type='password' 
+          value={password} 
+          onChange={(text) => setPassword(text)} 
+        />
+      </View>
+      <Button title='Login' />
+      <Button title='Register' type='outline' />
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+
+  },
+});
