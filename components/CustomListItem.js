9f25058eb8f6f3e6ba908b0e850c4487b33338c0
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-01-31 12:02:05 -0500

    Finished CustomListTime file

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,23 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import { Avatar, ListItem } from '@rneui/themed'
 import { db } from '../firebase'
-import { collection, query, orderBy, onSnapshot, addDoc } from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 
-const CustomListItem = ({ id, chatName, enterChat, messages }) => {
+const CustomListItem = ({ id, chatName, enterChat }) => {
   const [chatMessages, setChatMessages] = useState([]);
 
-  useEffect(() =>{
-    const unsubscribe = db
-      .collection('chats')
-      .doc(id).collection('messages')
-      .orderBy('timestamp', 'desc')
-      .onSnapshot(snapshot => 
-        setChatMessages(snapshot.docs.map(doc => doc.data()))
-      );
-    return unsubscribe;
-  });
 
+  useEffect(() => {
+    const messagesRef = collection(db, 'chats', id, 'messages');
+    const q = query(messagesRef, orderBy('timestamp', 'desc'));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setChatMessages(snapshot.docs.map(doc => doc.data()));
+    });
+
+    return unsubscribe;
+  }, [id]);
+
+  // useEffect(() =>{
+  //   const unsubscribe = db
+  //     .collection('chats')
+  //     .doc(id).collection('messages')
+  //     .orderBy('timestamp', 'desc')
+  //     .onSnapshot(snapshot => 
+  //       setChatMessages(snapshot.docs.map(doc => doc.data()))
+  //     );
+  //   return unsubscribe;
+  // });
 
   return (
     <ListItem 
